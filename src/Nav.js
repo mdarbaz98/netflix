7d89c478a79e0eece6d206f79b2f740cdcb1fcbb
mdarbaz98 <zainshaikh2468@gmail.com>
2022-02-06 20:35:14 +0530

    unused component remove

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import "./Nav.css";
 
