33f9c7f3a7e38628e39eebb1b7b910b5a8f15acf
mdarbaz98 <zainshaikh2468@gmail.com>
2022-02-06 20:25:31 +0530

    short changes in alt attribute

--- src/Nav.js
+++ src/Nav.js
@@ -1,31 +1,45 @@
-import React, { Component, useState,useEffect } from 'react';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import './Nav.css';
+import React, { Component, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import "./Nav.css";
 
 function Nav() {
-    const [show, setShow] = useState(false);
-    const history = useHistory();
-    const transitionNavBar = () => {
-        if(window.scrollY > 100){
-            setShow(true)
-        }else{
-            setShow(false)
-        }
+  const [show, setShow] = useState(false);
+  const history = useHistory();
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
     }
+  };
 
-    useEffect(() => {
-      window.addEventListener('scroll',transitionNavBar);
-      return () => window.removeEventListener('scroll',transitionNavBar);
-    }, []);
-    
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
 
-  return <div className={`Nav ${show && "Nav_black"}`}>
-  <div className='navbar'>
-      <img onClick={()=>{history.push('/')}} className='netflix-logo' src='./assets/images/580b57fcd9996e24bc43c529.png' alt='netflix-logo' />
-      <img onClick={()=>{history.push('/profile')}} className='netflix-avatar' src='./assets/images/flat,1000x1000,075,f.u2.jpg' alt='netflix-account-info-img' />
-  </div>
-</div>;
+  return (
+    <div className={`Nav ${show && "Nav_black"}`}>
+      <div className="navbar">
+        <img
+          onClick={() => {
+            history.push("/");
+          }}
+          className="netflix-logo"
+          src="./assets/images/580b57fcd9996e24bc43c529.png"
+          alt="netflix-logo"
+        />
+        <img
+          onClick={() => {
+            history.push("/profile");
+          }}
+          className="netflix-avatar"
+          src="./assets/images/flat,1000x1000,075,f.u2.jpg"
+          alt="netflix-account-info"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
-
--- src/Profilepage.js
+++ src/Profilepage.js
@@ -8,7 +8,7 @@ function Profilepage() {
     const user = useSelector(selectUser)
   return <div className='profilePage'>
         <Nav />
-     <img className='profile-image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTnqdc6oMUMq8X5Wx5hw5o7tgT2mfv8YgbUw&usqp=CAU" alt='profile-image' />
+     <img className='profile-image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTnqdc6oMUMq8X5Wx5hw5o7tgT2mfv8YgbUw&usqp=CAU" alt='profile' />
      <h2>{user.email}</h2>
      <button onClick={()=> {auth.signOut()}} className='logout-button'>Logout</button>
   </div>;
